1ce4df4d4d220f83e5b62ba25c2a000b3cf37fb1
Victoria <victoria.andujar10@gmail.com>
2025-09-16 03:01:39 -0300

    feat: testes de integracao e unidade em cursos e categorias

---
+++ src/test/java/br/com/alura/projeto/category/CategoryRepositoryTest.java
@@ -0,0 +1,33 @@
+package br.com.alura.projeto.category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CategoryRepositoryTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void existsByCode_shouldReturnTrue_whenCodeExists() {
+        Category category = new Category("Programação", "programacao", "#FFFFFF", 1);
+        entityManager.persist(category);
+
+        boolean result = categoryRepository.existsByCode("programacao");
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void existsByCode_shouldReturnFalse_whenCodeDoesNotExist() {
+        boolean result = categoryRepository.existsByCode("inexistente");
+
+        assertThat(result).isFalse();
+    }
+}
